d6029b13888021fa02d614fd3296ded15138f138
mihai <mihail.horanin@gmail.com>
2023-02-15 10:17:43 +0300

    homework

--- src/test/java/SingUpTest.java
+++ src/test/java/SingUpTest.java
@@ -39,17 +39,32 @@ public class SingUpTest {
     }
 
     @Test
-    public void zipCode6Digits() {
+    public void zipCodeNegative6Digits() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.sharelane.com/cgi-bin/register.py");
-        driver.findElement(By.name("zip_code")).sendKeys("111122");
+        driver.findElement(By.name("zip_code")).sendKeys("123456");
         driver.findElement(By.cssSelector("input[value=Continue]")).click();
         String actualError = driver.findElement(By.cssSelector("span[class=error_message]")).getText();
         assertEquals(actualError, "Oops, error on page. ZIP code should have 5 digits",
                 "Wrong error message shown");
         driver.close();
     }
+
+    @Test
+    public void zipCodeNegativeLetters() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.name("zip_code")).sendKeys("fffff");
+        driver.findElement(By.cssSelector("input[value=Continue]")).click();
+        String actualError = driver.findElement(By.cssSelector("span[class=error_message]")).getText();
+        assertEquals(actualError, "Oops, error on page. ZIP code should have 5 digits",
+                "Wrong error message shown");
+        driver.close();
+    }
+
+
 
     @Test
     public void successfulSignUp() {
@@ -68,5 +83,102 @@ public class SingUpTest {
         driver.close();
     }
 
+    @Test
+    public void negativeSingUpWithoutFirstName() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("last_name")).sendKeys("Last");
+        driver.findElement(By.name("email")).sendKeys("test@test.test");
+        driver.findElement(By.name("password1")).sendKeys("12345678");
+        driver.findElement(By.name("password2")).sendKeys("12345678");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        String actualError = driver.findElement(By.cssSelector("span[class=error_message]")).getText();
+        assertEquals(actualError,
+                "Oops, error on page. Some of your fields have invalid data or email was previously used",
+                "field is required");
+        driver.quit();
+    }
+
+    @Test
+    public void negativeSingUpFirstNameDigit() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("first_name")).sendKeys("1");
+        driver.findElement(By.name("last_name")).sendKeys("");
+        driver.findElement(By.name("email")).sendKeys("test@test.test");
+        driver.findElement(By.name("password1")).sendKeys("12345678");
+        driver.findElement(By.name("password2")).sendKeys("12345678");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        String actualError = driver.findElement(By.cssSelector("span[class=error_message]")).getText();
+        assertEquals(actualError,
+                "Oops, error on page. Some of your fields have invalid data or email was previously used",
+                "field is required");
+        driver.quit();
+    }
+
+
+    @Test
+    public void negativeSingUpWithoutEmail() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("first_name")).sendKeys("Name");
+        driver.findElement(By.name("last_name")).sendKeys("Last");
+        driver.findElement(By.name("password1")).sendKeys("12345678");
+        driver.findElement(By.name("password2")).sendKeys("12345678");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        String actualError = driver.findElement(By.cssSelector("span[class=error_message]")).getText();
+        assertEquals(actualError,
+                "Oops, error on page. Some of your fields have invalid data or email was previously used",
+                "box is required");
+        driver.quit();
+    }
+
+    @Test
+    public void negativeSingUpOnePassword() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("first_name")).sendKeys("Name");
+        driver.findElement(By.name("last_name")).sendKeys("Last");
+        driver.findElement(By.name("email")).sendKeys("test@test.test");
+        driver.findElement(By.name("password1")).sendKeys("12345678");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        String actualError = driver.findElement(By.cssSelector("span[class=error_message]")).getText();
+        assertEquals(actualError,
+                "Oops, error on page. Some of your fields have invalid data or email was previously used",
+                "box is required");
+        driver.quit();
+    }
+
+    @Test
+    public void negativeSingUpRepeatingPassword() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        driver.findElement(By.name("first_name")).sendKeys("Name");
+        driver.findElement(By.name("last_name")).sendKeys("Last");
+        driver.findElement(By.name("email")).sendKeys("test@test.test");
+        driver.findElement(By.name("password1")).sendKeys("1234567");
+        driver.findElement(By.name("password2")).sendKeys("12345678");
+        driver.findElement(By.cssSelector("input[value=Register]")).click();
+        String actualError = driver.findElement(By.cssSelector("span[class=error_message]")).getText();
+        assertEquals(actualError,
+                "Oops, error on page. Some of your fields have invalid data or email was previously used",
+                "boxes must be the same");
+        driver.quit();
+    }
+
+    @Test
+    public void negativeSingUpPasswordType() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://sharelane.com/cgi-bin/register.py?page=1&zip_code=11111");
+        String actualType = driver.findElement(By.name("password1")).getAttribute("type");
+        assertEquals(actualType, "password", "incorrect password type");
+        driver.quit();
+    }
 
 }
